176160a4161bef1410f788a5b09d7363b473e4f8
Daniel Kozlowski <daniel.kozlowski@debenhams.com>
2020-02-26 11:23:11 +0000

    Adding monitoring

---
+++ addons/monitoring/monitoring.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Preparing namespace for helm 3
+kubectl create namespace monitoring
+
+# Prometheus
+helm upgrade prometheus stable/prometheus \
+    --install \
+    --namespace=monitoring \
+    --set nodeExporter.enabled=false \
+    --set alertmanager.enabled=false \
+    --set pushgateway.enabled=false \
+    --values ./prometheus-values.yaml
+
+
+# Grafana
+kubectl -n monitoring create secret generic grafana-admin \
+    --from-literal="admin-user=admin" \
+    --from-literal="admin-password=admin"
+
+helm upgrade grafana stable/grafana \
+    --install \
+    --namespace=monitoring \
+    --values ./grafana-values.yaml
